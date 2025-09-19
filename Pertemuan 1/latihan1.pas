bed4e89e44e2b0566a936477b809c9c9ee03db5a
DeboraGraseliaPasaribu_251401010 <graselladebora@gmail.com>
2025-09-19 10:14:37 +0700

    Pertemuan 1 : Pengenalan Pemrograman dan Pascal

---
+++ Pertemuan 1/latihan1.pas
@@ -0,0 +1,32 @@
+
+program latihan;
+uses crt;
+
+var 
+Nama: string;
+Nim: string;
+Kom: char;
+Alamat: string;
+
+
+begin
+    clrscr;
+
+    write('Nama: ');
+    readln(Nama);
+
+    write('Nim:');
+    readln(Nim);
+
+    write('Kom: ');
+    readln(Kom);
+
+    write('Alamat:');
+    readln(Alamat);
+
+
+
+    writeln ('hallo, nama saya ', Nama, ' Nim saya ', Nim, ' saya di kom ', Kom , ' alamat saya di ', Alamat , ' salam kenal semua !')
+
+end.
+
