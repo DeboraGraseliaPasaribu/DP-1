cdf255bcf0e15fc1b88ec40e40b3482e8abe5dc6
DeboraGraseliaPasaribu_251401010 <graselladebora@gmail.com>
2025-11-07 09:41:41 +0700

    Pertemuan 6 : Prosedur dan Fungsi

---
+++ Pertemuan6/parameter.pas
@@ -0,0 +1,14 @@
+program Parameter;
+uses crt;
+procedure TampilkanNama (nama: string); //parameter formal
+begin
+    writeln ('Halo ', nama);
+end;
+var 
+    namaAktual: string;
+
+begin 
+    clrscr;
+    namaAktual := 'Edward';
+    TampilkanNama(namaAktual); 
+end.
